31f29244c5c55f2f5b6967b4cd8e7cea523bbc7c
oAlexChaves <68710954+oAlexChaves@users.noreply.github.com>
2021-03-31 22:01:22 -0300

    alterações na home page e formatação

---
+++ src/app/components/buttom-home/buttom-home.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-buttom-home',
+  templateUrl: './buttom-home.component.html',
+  styleUrls: ['./buttom-home.component.scss'],
+})
+export class ButtomHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
