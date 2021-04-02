6a5ce6248b29d2636f728ad907968599a0efe234
Matheus Rian <souzamatheusrian@gmail.com>
2021-04-02 12:15:15 -0300

    feat: app routes configured✨

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,23 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tab1/tab1.module').then(m => m.Tab1PageModule)
+    pathMatch: 'full',
+    loadChildren: () => import('./telaInicial/tab1.module').then(m => m.Tab1PageModule)
   },
   {
-    path: 'detalhes/treinamento-diaria',
+    path: 'detalhes/treinamento-diario',
     pathMatch: 'full',
-    loadChildren: () => import('./tab2/tab2.module').then(m => m.Tab2PageModule)
+    loadChildren: () => import('./treinamentoDiario/tab2.module').then(m => m.Tab2PageModule)
+  },
+  {
+    path: 'detalhes/alimentacao-diaria',
+    pathMatch: 'full',
+    loadChildren: () => import('./alimentacaoDiaria/tab3.module').then(m => m.Tab3PageModule)
+  },
+  {
+    path: 'detalhes/sono-diario',
+    pathMatch: 'full',
+    loadChildren: () => import('./sonoDiario/tab4.module').then(m => m.Tab4PageModule)
   },
 ];
 @NgModule({
